6d5cdb71b75b5a9dc783f9465f57098d08ac0f7c
Jameson Knoll <j.knoll621@gmail.com>
2019-05-12 22:54:27 -0400

    add methods to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do 
+
+  before do 
       @user = User.create(email: "test@test.com", password: 'asdfasdf', password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    end
+  end
+
+  describe "creation" do
 
     it "can be created" do
       expect(@user).to be_valid
@@ -14,6 +16,11 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+  describe "custom name methods" do
+    it "has a full name method that combines first name and last name" do 
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
